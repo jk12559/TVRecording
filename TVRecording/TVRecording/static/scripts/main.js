8eea2f0e4689b65bb1f9ef7d79cf224497de99ba
Jordan Kehrer <jkehrer@agi.com>
2017-01-11 02:32:42 -0500

    removed uneccessary functions from main.js

--- TVRecording/TVRecording/static/scripts/main.js
+++ TVRecording/TVRecording/static/scripts/main.js
@@ -18,59 +18,6 @@ function getAddShowForm() {
     var test = window.open('/addShow', 'Add Show', 'width=400,height=300,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=20,top=20');
 }
 
-function search() {
-    var searchText = document.getElementById('searchText').value;
-    var request = new XMLHttpRequest();
-    request.open('POST', '/showSearch');
-    request.setRequestHeader('Content-type', 'application/json');
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            var results = request.response;
-            displayResultsTable(results);
-        }
-    }
-    var requestData = { 'search': searchText };
-    request.send(JSON.stringify(requestData));
-}
-
-function displayResultsTable(data) {
-    var resultsList = JSON.parse(data)['data'];
-    var table = document.getElementById('searchResultsTable');
-    var selection = document.getElementById('showSelection')
-    var numRows = table.rows.length;
-
-    for (var i = 1; i < numRows; i++) {
-        table.deleteRow(1);
-        selection.remove(0);
-    }
-
-    for (var i = 0; i < resultsList.length; i++) {
-        var result = resultsList[i];
-        var row = table.insertRow(-1);
-        row.insertCell(-1).innerHTML = result['id'];
-        row.insertCell(-1).innerHTML = result['seriesName'];
-        row.insertCell(-1).innerHTML = result['network'];
-        row.insertCell(-1).innerHTML = result['overview'];
-
-        var option = document.createElement('option');
-        option.text = result['id'] + " - " + result['seriesName'];
-        selection.add(option);
-    }
-}
-
-function addToDB() {
-    var selection = document.getElementById('showSelection').value;
-    var request = new XMLHttpRequest();
-    request.open('POST', '/addToDB');
-    request.onreadystatechange = function () {
-        if (request.readyState == 4) {
-            getShowList();
-            getEpisodeList();
-        }
-    }
-    request.send(selection);
-}
-
 function getShowList() {
     var request = new XMLHttpRequest();
     request.open('GET', '/showList');
